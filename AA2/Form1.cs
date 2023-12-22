566a2c8b1dd7d6657cea06843e60c1a04d33a4b0
Stekoks <Stekoks@DESKTOP-HSE7N52>
2023-12-22 03:46:25 +0200

    Add project files.

---
+++ AA2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace AA2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Load += new System.EventHandler(this.Form1_Load);
+            button1.Click += button1_Click;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text.Trim();
+            if (string.IsNullOrEmpty(url))
+            {
+                MessageBox.Show("Please enter a valid URL.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    string content = streamReader.ReadToEnd();
+                    Dictionary<string, int> wordFrequency = CreateWordFrequency(content);
+                    listBox1.Items.Clear();
+                    foreach (var pair in wordFrequency)
+                    {
+                        listBox1.Items.Add($"Word: {pair.Key}, Amount: {pair.Value}");
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show("Web Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private Dictionary<string, int> CreateWordFrequency(string content)
+        {
+            string[] words = content.Split(new char[] { ' ', '\n', '\r', '\t', '.', ',', ';', ':', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> wordFrequency = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            foreach (string word in words)
+            {
+                if (wordFrequency.ContainsKey(word))
+                {
+                    wordFrequency[word]++;
+                }
+                else
+                {
+                    wordFrequency[word] = 1;
+                }
+            }
+            return wordFrequency;
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
